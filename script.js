0d60bdb82306bc7b5a583ea0e8705bb02f8df4c5
Cтас <90937465+sfirsovdev@users.noreply.github.com>
2022-04-22 22:31:29 +0300

    Финал

---
+++ script.js
@@ -0,0 +1,16 @@
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+}
+
+
+const bnt = document.querySelector('#btn');
+
+btn.addEventListener('mouseenter', () => {
+    btn.style.left = `${random(0, 90)}%`;
+    btn.style.top = `${random(0, 90)}%`;
+});
+
+btn.addEventListener('click', () => {
+    alert('Ладно, пойду сделаю :)');
+});
